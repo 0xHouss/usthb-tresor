df4837584c663c9d5b559a6fc7ebd0243525c1bd
0xHouss <tkthoussam@gmail.com>
2025-06-06 16:49:45 +0100

    feat(env): update env schema

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,18 +1,13 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  GOOGLE_CLIENT_ID: z
-    .string({ required_error: "GOOGLE_CLIENT_ID is required" })
-    .min(1, "GOOGLE_CLIENT_ID is required"),
-  GOOGLE_CLIENT_SECRET: z
-    .string({ required_error: "GOOGLE_CLIENT_SECRET is required" })
-    .min(1, "GOOGLE_CLIENT_SECRET is required"),
-  AUTH_URL: z
-    .string({ required_error: "NEXTAUTH_URL is required" })
-    .min(1, "NEXTAUTH_URL is required"),
-  AUTH_SECRET: z
-    .string({ required_error: "AUTH_SECRET is required" })
-    .min(1, "AUTH_SECRET is required"),
+  DATABASE_URL: z.string().url(),
+
+  AUTH_GOOGLE_ID: z.string().min(1),
+  AUTH_GOOGLE_SECRET: z.string().min(1),
+  AUTH_SECRET: z.string().min(1),
+
+  GOOGLE_DRIVE_FOLDER_ID: z.string().min(1),
 })
 
 const ENV = envSchema.parse(process.env)
