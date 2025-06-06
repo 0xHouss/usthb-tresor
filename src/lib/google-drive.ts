3733697edec3ecc501f9084da8fcba5dbcb45b78
0xHouss <tkthoussam@gmail.com>
2025-06-06 16:45:56 +0100

    refactor(google-drive): switch from service account to oauth

--- src/lib/google-drive.ts
+++ src/lib/google-drive.ts
@@ -1,8 +1,17 @@
-import { google } from "googleapis";
+import fs from 'fs';
+import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-  keyFile: "google-service-account.json",
-  scopes: ["https://www.googleapis.com/auth/drive"],
-});
+const credentials = JSON.parse(fs.readFileSync('credentials.json', 'utf8'));
+const token = JSON.parse(fs.readFileSync('token.json', 'utf8'));
 
-export const drive = google.drive({ version: "v3", auth });
+const { client_secret, client_id, redirect_uris } = credentials.web;
+
+const oAuth2Client = new google.auth.OAuth2(
+  client_id,
+  client_secret,
+  redirect_uris[0]
+);
+
+oAuth2Client.setCredentials(token);
+
+export const drive = google.drive({ version: 'v3', auth: oAuth2Client });
