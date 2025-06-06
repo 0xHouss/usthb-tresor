119ca060f2ecfd3990943a46043ceebcd6fe94e2
0xHouss <tkthoussam@gmail.com>
2025-06-06 16:49:26 +0100

    feat(utils): create getFileUrl, getFileDownloadUrl, isEnumValue utility functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { z } from "zod";
 
 export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
 
@@ -11,3 +12,10 @@ export function getInitials(name: string) {
 
   return initials || "?";
 }
+
+export function isEnumValue<T extends Record<string, string>>(enumObj: T, value: string): value is T[keyof T] {
+  return Object.values(enumObj).includes(value);
+}
+
+export const getFileUrl = (fileId: string) => `https://drive.google.com/file/d/${fileId}/view?usp=sharing`
+export const getFileDownloadUrl = (fileId: string) => `https://drive.google.com/uc?export=download&id=${fileId}`
